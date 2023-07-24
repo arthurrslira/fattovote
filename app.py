88c03383b21e5c48c7ce797116f36d38ac5f6828
arthurrslira <139889835+arthurrslira@users.noreply.github.com>
2023-07-24 16:14:50 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -102,11 +102,8 @@ st.altair_chart(chart, use_container_width=True)
 
 # Adicionando a aba lateral com a seção "Sobre"
 st.sidebar.title('Sobre')
-st.sidebar.info('Este é um projeto desenvolvido por Arthur Lira e Filipe Pontes para a disciplina de Análise e Visualização de Dados da Pós-Graduação de Engenharia e Análise de Dados da CESAR School, turma 2023.1')
-
-st.sidebar.title('Insights')
-st.sidebar.info('Através do dashboard e dos gráficos que podem ser gerados a partir dos filtros, podemos perceber uma distribuição partidária bem interessante: 1) a pauta econômica (reforma tributária, arcabouço fiscal) recebeu apoio dos partidos de centro e centro-direita, como PP, PSD, Republicanos e União Brasil; 2) no entanto, o marco temporal das terras indígenas dividiu a Câmara entre direita e esquerda e mesmo partidos com ministérios no governo Lula votaram a favor do projeto, o qual o governo se posicionou contra; 3) entre os partidos com ministérios no governo, o PSOL, partido mais à esquerda com representação parlamentar, foi o que proporcionalmente menos apoiou a reforma tributária.')
+st.sidebar.info('Dashboard da Fatto Inteligência Política sobre votações nominais na Câmara dos Deputados. Acesso restrito para clientes.')
 
 # Adicionando a logo
-logo = "marca_cesar_school.png"
+logo = "fatto_marca.png"
 st.sidebar.image(logo, use_column_width=True)
