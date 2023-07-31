89fed9ce8d7fce62e8f079d343f38267ecde8372
arthurrslira <139889835+arthurrslira@users.noreply.github.com>
2023-07-31 15:22:21 -0300

    Add files via upload

--- pages/4_📞_Contato.py
+++ pages/4_📞_Contato.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
      page_title="FattoVote - Contato",
-     page_icon="📞",
+     page_icon="https://i.ibb.co/x1Y9wJh/Monograma-Verde.png",
      layout="wide",
      initial_sidebar_state="expanded",
 )
