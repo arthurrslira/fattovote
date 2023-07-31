911bce7fb7cd2b0f0438ba7bc51bdc35a086e4a0
arthurrslira <139889835+arthurrslira@users.noreply.github.com>
2023-07-31 10:20:37 -0300

    Add files via upload

--- pages/5_📞_Contato.py
+++ pages/5_📞_Contato.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 
 imagem = "marca_fatto.png"
-st.sidebar.image(imagem, use_column_width=False, width=100)
+st.sidebar.image(imagem, use_column_width=False, width=300)
 
 st.header("📞 Contato")
 
