c5dd0e12ec00245a2fa54842fad7312a55485c8d
arthurrslira <139889835+arthurrslira@users.noreply.github.com>
2023-07-31 10:28:29 -0300

    Add files via upload

---
+++ pages/3_📝_Sobre.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+st.set_page_config(
+     page_title="FattoVote - Sobre",
+     page_icon="📝",
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
+
+imagem = "marca_fatto.png"
+st.sidebar.image(imagem, use_column_width=False, width=300)
+
+st.header("📝 Sobre")
+
+texto = """
+    <p style="text-align: justify;">Dashboard da <strong>Fatto Inteligência Política</strong> sobre votações nominais na Câmara dos Deputados. Acesso restrito para clientes."
+    </p>
+    """
+
+st.header("🎲 Origem dos Dados")
+
+st.subheader("Dataset")
+
+texto = """
+    <p style="text-align: justify;">
+    Os dados foram extraídos através do site da <strong>Câmara dos Deputados</strong>. (<a href="https://www2.camara.leg.br/atividade-legislativa/plenario/relatorios-da-atividade-legislativa/dados-de-votacoes-nominais" target="_blank">link</a>)
+    </p>
+    """
+
+st.markdown(texto, unsafe_allow_html=True)
+
+st.subheader("Imagens dos Parlamentares")
+
+texto2 = """
+    <p style="text-align: justify;">
+    As imagens dos parlamentares foram extraídas diretamente do site da Câmara dos Deputados. (<a href="https://www.camara.leg.br/deputados/quem-sao">link</a>)
+    </p>
+    """
+
+st.markdown(texto2, unsafe_allow_html=True)
+
+st.markdown(texto, unsafe_allow_html=True)
+
+
