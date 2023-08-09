0abe8f18f56ff2c9d338dcf2933dd97a2571a919
arthurrslira <139889835+arthurrslira@users.noreply.github.com>
2023-08-09 20:54:00 -0300

    Add files via upload

--- pages/2_📊_Senado.py
+++ pages/2_📊_Senado.py
@@ -29,7 +29,7 @@ qrcode = "qrcode.png"
 st.sidebar.image(qrcode, use_column_width=False, width=300)
 
 st.markdown(
-"<h2 style='text-align: center; background-color: #307c5c; color: white; padding: 16px;'>Votações Nominais no Senado Federal - 2023</h2>",
+"<h2 style='text-align: center; background-color: #005c9e; color: white; padding: 16px;'>Votações Nominais no Senado Federal - 2023</h2>",
 unsafe_allow_html=True
 )
 
