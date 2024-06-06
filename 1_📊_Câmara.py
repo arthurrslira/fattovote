ffcb71d614769ac79a44fcc083f90939823577b6
arthurrslira <139889835+arthurrslira@users.noreply.github.com>
2024-06-06 10:46:23 -0300

    Add files via upload

--- 1_📊_Câmara.py
+++ 1_📊_Câmara.py
@@ -6,14 +6,23 @@ import streamlit.components.v1 as components
 import yaml 
 from yaml.loader import SafeLoader
 
-df_img = pd.read_excel('deputados_imagens.xlsx')
+# Função para carregar os dados com cache
+@st.cache_data
+def load_data(file):
+    return pd.read_excel(file)
 
-def obter_url_imagem(nome_parlamentar):
+# Função para obter a URL da imagem com cache
+@st.cache_data
+def obter_url_imagem(nome_parlamentar, df_img):
     deputado_filtrado = df_img[df_img['Nome do Deputado'] == nome_parlamentar]
     if not deputado_filtrado.empty:
         return deputado_filtrado['Link da Imagem'].iloc[0]
     else:
         return 'https://www.camara.leg.br/tema/assets/images/foto-deputado-sem-foto-peq.png'
+
+# Carregar dados
+df_img = load_data('deputados_imagens.xlsx')
+df = load_data('camara_deputados_votacoe.xlsx')
 
 st.set_page_config(
      page_title="FattoVote",
@@ -43,25 +52,22 @@ authenticator = stauth.Authenticate(
 #st.sidebar.write(f'Bem-vindo *{name}*')
 #authenticator.logout('Logout', 'sidebar')
 
-df = pd.read_excel('camara_deputados_votacoe.xlsx', header=[0, 1])
 parlamentares = sorted(df['Votação', 'Parlamentar'].unique())
 partidos_unicos = sorted(df['Unnamed: 0_level_0', 'Partido'].unique())
 
-
+# Carregar imagens e QR code
 imagem = "marca_fatto.png"
 st.sidebar.image(imagem, use_column_width=False, width=300)
-
 qrcode = "qrcode.png"
 st.sidebar.image(qrcode, use_column_width=False, width=300)
 
 st.markdown(
-"<h2 style='text-align: center; background-color: #307c5c; color: white; padding: 16px;'>Votações Nominais na Câmara dos Deputados - 2023</h2>",
-unsafe_allow_html=True
+    "<h2 style='text-align: center; background-color: #307c5c; color: white; padding: 16px;'>Votações Nominais na Câmara dos Deputados - 2023</h2>",
+    unsafe_allow_html=True
 )
 
 projetos = df.columns.get_level_values(0)[2:].unique()
 df_projetos = pd.DataFrame(projetos, columns=['projeto'])
-
 df_projetos['projeto_data'] = df_projetos['projeto'].apply(lambda x: x.split("-", 1)[0].strip())
 df_projetos['projeto_nome'] = df_projetos['projeto'].apply(lambda x: x.split("-", 1)[1].strip())
 
@@ -82,21 +88,17 @@ projetos_principais = [
 primeiros_valores = [tupla[0] for tupla in projetos_principais]
 
 st.subheader('Projeto:')
-
 projeto_selecionado2 = st.selectbox('Selecione o projeto', primeiros_valores)
 
 st.subheader('Partidos:')
-
 if st.checkbox("Todos", value=True):
     partidos_selecionados = partidos_unicos
 else:
     partidos_selecionados = st.multiselect('Selecione os partidos', partidos_unicos, default=partidos_unicos)
 
 st.subheader('Parlamentares:')
-
 parlamentares_selecionados = df.loc[df[('Unnamed: 0_level_0', 'Partido')].isin(partidos_selecionados), ('Votação', 'Parlamentar')].unique()
 selecionar_parlamentar = st.selectbox('Selecione um parlamentar', ['Todos'] + sorted(parlamentares_selecionados))
-parlamentar_selecionado = [selecionar_parlamentar]
 
 if selecionar_parlamentar == 'Todos':
     parlamentar_selecionado = parlamentares
@@ -109,23 +111,22 @@ for tupla in projetos_principais:
         break
 
 df2 = df[['Unnamed: 0_level_0', 'Votação', projeto_selecionado]]
-
 df_filtrado = df2[
     (df2[('Unnamed: 0_level_0', 'Partido')].isin(partidos_selecionados)) &
     (df2[('Votação', 'Parlamentar')].isin(parlamentar_selecionado))
 ]
 
-if not parlamentar_selecionado:
+if df_filtrado.empty:
     st.markdown("Nenhum parlamentar encontrado.")
 
 df_filtrado.columns = ["Partido", "Parlamentar", "Voto", "Orientação"]
-
 df_filtrado = df_filtrado[df_filtrado['Voto'] != '-']
 
-df_filtrado["Imagem"] = df_filtrado["Parlamentar"].apply(obter_url_imagem)
+# Aplicar a função de URL da imagem
+df_filtrado["Imagem"] = df_filtrado["Parlamentar"].apply(lambda x: obter_url_imagem(x, df_img))
 df_filtrado = df_filtrado[["Imagem", "Parlamentar", "Partido", "Voto", "Orientação"]]
-st.markdown(projeto_selecionado)
 
+st.markdown(projeto_selecionado)
 st.dataframe(
     df_filtrado,
     column_config={
@@ -136,18 +137,15 @@ st.dataframe(
 )
 
 grouped_data = df_filtrado.groupby(["Partido", "Voto"]).size().unstack(fill_value=0)
-
 party_sums = grouped_data.sum(axis=1)
 sorted_parties = party_sums.sort_values(ascending=False).index
-
 sorted_data = grouped_data.loc[sorted_parties]
 
-percentages = sorted_data.apply(lambda row: (row / row.sum())*100, axis=1).round(2)
-
+percentages = sorted_data.apply(lambda row: (row / row.sum()) * 100, axis=1).round(2)
 df_long = percentages.reset_index().melt(id_vars='Partido', var_name='Voto', value_name='Percentual')
 
 colors = alt.Scale(domain=['Sim', 'Não', 'Não votou', 'Abstenção'],
-                range=['#90EE90', '#FFA07A', '#D3D3D3', '#D2B48C'])
+                   range=['#90EE90', '#FFA07A', '#D3D3D3', '#D2B48C'])
 
 chart = alt.Chart(df_long).mark_bar().encode(
     x='Partido',
@@ -162,16 +160,14 @@ chart = alt.Chart(df_long).mark_bar().encode(
 st.altair_chart(chart, use_container_width=True)
 
 #elif authentication_status is False:
-#    st.error('Username/senha está incorreta')
+#    st.error('Username/password is incorrect')
 #elif authentication_status is None:
-#    st.warning('Por favor insira seu nome de usuário e senha')
+#    st.warning('Please enter your username and password')
 
-#    try:
-#        if authenticator.register_user('Primeiro Login', preauthorization=True):
-#            st.success('User registered successfully')
- #   except Exception as e:
-#        st.error(e)
-
-# Saving config file
-#with open('config.yaml', 'w') as file:
-#    yaml.dump(config, file, default_flow_style=False)
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
